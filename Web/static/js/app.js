ec7504b849abe28a9ad345e309e8e8d38abd329f
Jessica Fett <jessfett@Jessicas-MacBook-Air.local>
2021-02-01 16:59:30 -0500

    Updated

--- Web/static/js/app.js
+++ Web/static/js/app.js
@@ -6,7 +6,7 @@ var urlinjuries = "http://127.0.0.1:5000/injuries"
 
 
 // Load data from hours-of-tv-watched.csv
-d3.csv("../Data/CrashImpacts.csv").then(function(boroughData) {
+d3.csv("..ETL/Data/CrashImpacts.csv").then(function(boroughData) {
 
 	console.log(boroughData);
 	var personinjured = boroughData.PersonsInjured; 
